bf154a09059e77a6fae2520198e3bdd9094c9a9f
NeuronaBens <neuronabens@gmail.com>
2023-04-28 14:47:33 -0500

    update of link location

---
+++ assets/card-links-config.js
@@ -0,0 +1,50 @@
+//GVC = Global Variable of Configuration
+
+GVC_ExtraDeckCardsLinks =`
+https://media.discordapp.net/attachments/1095765068051922996/1096483489492246709/Reborning_Magician.jpeg
+https://media.discordapp.net/attachments/1095765068051922996/1096483489169281024/Raigeki_Magician.jpeg
+https://media.discordapp.net/attachments/1095765068051922996/1096604365118242868/Alluring_Darkness_Magician_3.jpeg
+`;
+
+GVC_DeckCardsLinks = `
+https://media.discordapp.net/attachments/1086535051350654979/1088516777799061614/71d633mhCWL.jpg?width=590&height=860, poli
+https://media.discordapp.net/attachments/1086535051350654979/1088516777799061614/71d633mhCWL.jpg?width=590&height=860, poli
+https://media.discordapp.net/attachments/1086535051350654979/1088516936696090806/1083505.jpg?width=590&height=860, dark mag
+https://media.discordapp.net/attachments/1086535051350654979/1088516936696090806/1083505.jpg?width=590&height=860, dark mag
+https://static.wikia.nocookie.net/yugioh/images/3/3a/LusterDragon-SDKS-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20161021201147, luster dragon
+https://static.wikia.nocookie.net/yugioh/images/3/3a/LusterDragon-SDKS-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20161021201147, luster draghon
+https://static.wikia.nocookie.net/yugioh/images/6/60/Marshmallon-YS18-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20180712164437, marshemeloo
+https://static.wikia.nocookie.net/yugioh/images/6/60/Marshmallon-YS18-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20180712164437, marshemeloo
+https://static.wikia.nocookie.net/yugioh/images/8/8b/NimbleMomonga-EGO1-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20220705044731, nimble momonga
+https://static.wikia.nocookie.net/yugioh/images/8/8b/NimbleMomonga-EGO1-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20220705044731, nimble momonga
+https://static.wikia.nocookie.net/yugioh/images/8/8b/NimbleMomonga-EGO1-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20220705044731, nimble momonga
+https://static.wikia.nocookie.net/yugioh/images/b/b3/Tragoedia-SDSA-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20200715184252, tragoedia
+https://static.wikia.nocookie.net/yugioh/images/b/b3/Tragoedia-SDSA-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20200715184252, tragoedia
+https://static.wikia.nocookie.net/yugioh/images/0/07/BreakertheMagicalWarrior-EGS1-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20220706050841, BreakertheMagicalWarrior
+https://static.wikia.nocookie.net/yugioh/images/0/07/BreakertheMagicalWarrior-EGS1-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20220706050841, BreakertheMagicalWarrior
+https://static.wikia.nocookie.net/yugioh/images/e/ef/AshBlossomJoyousSpring-SDCB-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20220930031434, AshBlossomJoyousSpring
+https://product-images.tcgplayer.com/266310.jpg, cyber dragon
+https://preview.redd.it/c9a6ujog37az.jpg?auto=webp&s=8a00db6888b785ad407a9b7cd26a812046da4564, cyber dinosaur
+https://ms.yugipedia.com//1/19/DragonIce-BP02-EN-C-1E.png, dragon ice
+https://static.wikia.nocookie.net/yugioh/images/a/a5/Amarylease-BP02-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20130713042614, amarylease
+https://static.wikia.nocookie.net/yugioh/images/c/cc/BlueDragonSummoner-SS01-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20190128200008, bluedragon summoner
+https://static.wikia.nocookie.net/yugioh/images/c/cc/BlueDragonSummoner-SS01-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20190128200008, buedragon summoner
+https://static.wikia.nocookie.net/yugioh/images/c/cd/TwinTwisters-MGED-EN-PGR-1E.png/revision/latest/scale-to-width-down/300?cb=20211227154031, twin twisters
+https://static.fandomspot.com/images/09/19958/10-monster-reincarnation-card-ygo.jpg, monster reincarnation
+https://static.wikia.nocookie.net/yugioh/images/e/e1/Raigeki-MGED-EN-PGR-1E.png/revision/latest/scale-to-width-down/300?cb=20211227152709, raigeki
+https://static.wikia.nocookie.net/yugioh/images/4/40/MonsterReborn-EGO1-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20210920164521, monster reborn
+https://static.wikia.nocookie.net/yugioh/images/8/85/AllureofDarkness-BROL-EN-UR-1E.png/revision/latest/scale-to-width-down/300?cb=20220111225946, allure of darkness
+https://static.wikia.nocookie.net/yugioh/images/c/c6/SolemnJudgment-MAZE-EN-SR-1E.png/revision/latest/scale-to-width-down/300?cb=20230317011855, solemn judgement
+https://static.wikia.nocookie.net/yugioh/images/a/ad/BreakthroughSkill-SR02-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20160706183604, breakthroug skill
+https://product-images.tcgplayer.com/167206.jpg, drowning mirror force
+https://images.saymedia-content.com/.image/c_limit%2Ccs_srgb%2Cq_auto:eco%2Cw_476/MTc0NDYwODUwMDA5NDE3MzUw/best-yu-gi-oh-trap-card-list.webp, balance of judgement
+https://static.fandomspot.com/images/05/14477/16-solemn-warning-yugioh-card.jpg, solemn warning
+https://static.fandomspot.com/images/06/16348/04-cauldron-of-the-old-man-card.jpg, cauldron of the old man
+https://ms.yugipedia.com//9/95/MysticalSpaceTyphoon-HAC1-EN-DUPR-1E.png, Mystical spce typhoon
+https://ms.yugipedia.com//8/82/LevelLimitAreaB-AP07-EN-SR-UE.png, Level Limit - Area B
+https://cdn.cardsrealm.com/images/cartas/mp18-2018-mega-tin-mega-pack/en/med/mythical-beast-master-cerberus-en185.png?7579?&width=590, mythical beast master cerberus
+https://static.wikia.nocookie.net/yugioh/images/d/d4/MythicalBeastCerberus-BP03-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20140803171530, mythical beast cerberus
+https://static.wikia.nocookie.net/yugioh/images/d/d4/MythicalBeastCerberus-BP03-EN-C-1E.png/revision/latest/scale-to-width-down/300?cb=20140803171530, mythical beast cerberus
+https://media.discordapp.net/attachments/1095765068051922996/1095765110703788172/Uctumuludonte_Morabaskar.jpeg?width=278&height=406, Uctumuludonte Morabaskar
+https://media.discordapp.net/attachments/1095765068051922996/1096131103607238746/The_destiny_of_Morabaskar.jpeg, the destiny of Morabaskar
+`;
